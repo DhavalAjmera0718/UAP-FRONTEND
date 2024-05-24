c577617d0ab92b62cc3b2afbe130c95a778ed978
Darshan10012003 <darshanvaghasiya586@gmail.com>
2024-05-24 12:19:51 +0530

    Darshan code push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { RegisterComponent } from './Component/register/register.component';
 import { LoginComponent } from './Component/login/login.component';
 import { RegisterfirstComponent } from './Component/registerfirst/registerfirst.component';
 import { DashboardComponent } from './Component/dashboard/dashboard.component';
+import { HomeComponent } from './Component/home/home.component';
 
 const routes: Routes = [
     {path:'',redirectTo:'/registerfirst',pathMatch :'full'},
     {path:'register',component:RegisterComponent},
     {path:'registerfirst',component:RegisterfirstComponent},
     {path:'login',component:LoginComponent},
-    {path:'dashborad',component:DashboardComponent}
+    {path:'dashborad',component:DashboardComponent},
+    {path:'home',component:HomeComponent}
 ];
 
 @NgModule({
